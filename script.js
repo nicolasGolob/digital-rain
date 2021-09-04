270f7ba709be47efca28e9439304b1f9fc303aef
Nicolas Golob <nicolasgolob@gmail.com>
2021-09-04 15:35:16 +0200

    digital rain v1

---
+++ script.js
@@ -0,0 +1,24 @@
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
+let string = "こしあぶら、山うど、タラの芽、筍、春の香りがたまりません。";
+let matrix = string.split('');
+let font = 10;
+let column = width / font;
+let array = [];
+
+for(let i = 0; i < column; i++) {
+    array[i] = 1;
+}
+
+const draw = () => {
+    context.fillStyle = 'rgba(0,0,5,0.04)';
+    context.fillStyle = '#00FF05';
+    context.font = `${font}px system-ui`;
+    //system font
+    context.fillRect(0, 0, width, height);
+ 
+    
+}
+
