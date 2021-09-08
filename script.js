9d09af2ad1ac161b8e5a7934ac57d295c099f6f6
Nicolas Golob <nicolasgolob@gmail.com>
2021-09-08 17:21:51 +0200

    Js Adjustments

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let canvas = document.querySelector('canvas');
 let context = canvas.getContext('2d');
 let width = canvas.width = window.innerWidth;
 let height = canvas.height = window.innerHeight;
-let string = "こしあぶら、山うど、タラの芽、筍、春の香りがたまりません。";
-let matrix = string.split('');
-let font = 10;
+let string = 'こしあぶら、山うど、タラの芽、筍、春の香りがたまりません。';
+let matrix = string.split("");
+let font = 12;
 let column = width / font;
 let array = [];
 
@@ -13,10 +13,10 @@ for(let i = 0; i < column; i++) {
 }
 const draw = () => {
     context.fillStyle = 'rgba(0,0,5,0.04)';
+    context.fillRect(0, 0, width, height);
     context.fillStyle = '#00FF05';
     context.font = `${font}px system-ui`;
     //system font
-    context.fillRect(0, 0, width, height);
     for(let i = 0; i < array.length; i++) {
         let txt = matrix[Math.floor(Math.random() * matrix.length)];
         context.fillText(txt, i * font, array[i] * font);
